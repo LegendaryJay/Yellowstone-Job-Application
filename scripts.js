fafa311579d3139137984112d930abbd4665378d
Dan <amorecbellum@gmail.com>
2022-09-07 16:09:23 -0500

    School Summary

--- scripts.js
+++ scripts.js
@@ -34,29 +34,55 @@ function displaySummary() {
     displayNewline()
 
     displayTitle("Employment")
+    displayThis("Job 1", "", true)
+    displayThis("Company", $("#e1-company").val());
+    displayThis("Address", $("#e1-street").val());
+    displayThis("", $("#e1-city").val() + " " + $("#e1-state").val() + " " + $("#e1-zip").val());
+    displayThis("", $("#e1-country").val());
+    displayThis("Phone", $("#e1-phone").val());
+    displayThis("Position", $("#e1-position").val());
+    displayThis("Duties", $("#e1-duty").val());
+    displayThis("Dates Working", $("#e1-start-date").val() + "-" + $("#e1-end-date").val());
+    displayThis("Reason for leaving", $("#e1-leave-reason").val());
+    displayThis("Permission", "We can " + ($("e1-contact-permission").is(':checked') ? '' : "not") + " contact this employer");
+    displayNewline()
+
+    displayTitle("Education");
+    displayThis("School 1", "", true)
+    displayThis("School", $('#school-1-name').val());
+    displayThis("Location", $('#school-1-city').val() + ", " + $('#school-1-state').val());
+    displayThis("", $('#school-1-country').val());
+    displayThis("Degree", $('#school-1-degree').val());
+    displayThis("Field of Study", $('#school-1-field').val());
+    displayThis("Graduated", $('#school-1-graduated').is(':checked') ? 'True' : 'False');
+    displayThis("Other Skills", $('#school-additional-skills'))
+    displayNewline()
+
+    
+
+
 }
 
 
 function displayTitle(title) {
-    $('#output-confirmation').append(
-        "<h1 class='h4'>" + title + "</h1>"
-    )
+    $('#output-confirmation').append("<h1 class='h4'>" + title + "</h1>")
 }
 
 function displayNewline() {
-    $('#output-confirmation').append(
-        "<hr>"
-    )
+    $('#output-confirmation').append("<hr class='my-3'>")
 }
 
-function displayThis(title, value) {
+function displayRow1(value) {
+    return "<div class='col-2 text-primary'>" + value + "</div>"
+}
 
-    $('#output-confirmation').append(
-        "<div class='row'>" +
-        "<div class='col-2 text-primary'>" + title + "</div>" +
-        "<div class='col-10'>" + (value.trim() === '' ? "---" : value) + "</div>" +
-        "</div>"
-    )
+function displayRow2(value) {
+    return "<div class='col-10'>" + value + "</div>"
+}
+
+function displayThis(title, value, ignoreValidation) {
+    const value2 = ignoreValidation ? value : (value.trim() === '' ? "---" : value);
+    $('#output-confirmation').append("<div class='row'>" + displayRow1(title) + displayRow2(value2) + "</div>")
 }
 
 function showJobs() {
